89896f6a0f649112105c42405833511279c54e07
rewolf <andrew.flower@gmail.com>
2020-10-26 01:12:22 +0900

    Add multipart code

--- src/main/java/CalendarMailer.java
+++ src/main/java/CalendarMailer.java
@@ -12,8 +12,7 @@ import javax.activation.DataSource;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.*;
 import javax.mail.util.ByteArrayDataSource;
 import java.util.Date;
 import java.util.Properties;
@@ -26,10 +25,9 @@ public class CalendarMailer {
         final Session mailSession = Session.getDefaultInstance(properties);
 
         // Calendar as message content
-        final MimeMessage message = new MimeMessage(mailSession);
-        final DataSource source = new ByteArrayDataSource(generateICalData(), "text/calendar; charset=UTF-8");
-        message.setDataHandler(new DataHandler(source));
-        message.setHeader("Content-Type", "text/calendar; charset=UTF-8; method=REQUEST");
+        final MimeMessage message = createMultipartTextFileAndCalendar(mailSession);
+        // Simple Calendar-only mail:
+//        final MimeMessage message = createICalPart(new MimeMessage(mailSession));
 
         // send
         final InternetAddress toAddress = new InternetAddress("not.amazing.attendee@domain", "Not Amazing Attendee");
@@ -38,6 +36,29 @@ public class CalendarMailer {
         message.setSender(fromAddress);
 
         Transport.send(message);
+    }
+
+    private static MimeMessage createMultipartTextFileAndCalendar(final Session mailSession) throws Exception {
+        final MimeMessage message = new MimeMessage(mailSession);
+        final MimeMultipart mixedMultipart = new MimeMultipart("mixed");
+        mixedMultipart.addBodyPart(createICalPart(new MimeBodyPart()));
+        mixedMultipart.addBodyPart(createTextAttachment());
+        message.setContent(mixedMultipart);
+        return message;
+    }
+
+    public static MimeBodyPart createTextAttachment() throws Exception {
+        final MimeBodyPart textAttachmentPart =  new MimeBodyPart();
+        textAttachmentPart.setContent("no stuffs", "text/plain; charset=UTF-8");
+        textAttachmentPart.addHeader("Content-Disposition", "attachment; filename=fun.txt");
+        return textAttachmentPart;
+    }
+
+    public static <T extends MimePart> T createICalPart(T mimePartForCalendar) throws Exception {
+        final DataSource source = new ByteArrayDataSource(generateICalData(), "text/calendar; charset=UTF-8");
+        mimePartForCalendar.setDataHandler(new DataHandler(source));
+        mimePartForCalendar.setHeader("Content-Type", "text/calendar; charset=UTF-8; method=REQUEST");
+        return mimePartForCalendar;
     }
 
 
